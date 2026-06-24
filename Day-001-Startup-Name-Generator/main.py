ef0752cee6410f9ee741d103791e354259b233c7
Shivam Kumar <pshiva5394m@gmail.com>
2026-06-24 16:07:14 +0530

    feat: implement tech startup name generator with newline formatting

---
+++ Day-001-Startup-Name-Generator/main.py
@@ -0,0 +1,6 @@
+#generate tech startup name
+
+print("Welcome to the Startup Name Generator.")
+tech_word = input("What's your favorite futuristic tech word?\n")
+food_name = input("What's your favorite food?\n")
+print("Your tech startup name could be " + tech_word + " " + food_name)
